c5b5b348486af4d150fda6cf98f64a58ceb051bf
sarveshgk10 <35034236+sarveshgk10@users.noreply.github.com>
2021-02-10 19:34:16 +0530

    Add files via upload

---
+++ push.js
@@ -0,0 +1,7 @@
+clevertap.notifications.push({
+  "titleText":'Would you like to receive Push Notifications?',
+  "bodyText":'We promise to only send you relevant content and give you updates on your transactions',
+  "okButtonText":'Sign me up!',
+  "rejectButtonText":'No thanks',
+  "okButtonColor":'#f28046'
+});
